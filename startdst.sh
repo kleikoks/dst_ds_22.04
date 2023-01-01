5980a45dee7589e4166e2b5cc56b723c55b35d65
kleikoks <kleikoks.py@gmail.com>
2023-01-01 19:49:26 +0200

    update startdst.sh, add server config

--- startdst.sh
+++ startdst.sh
@@ -3,7 +3,7 @@
 install_dir="$HOME/Steam/dstserver"
 cluster_name="MyDediServer"
 
-run_shared=(./$install_dir/bin64/dontstarve_dedicated_server_nullrenderer_x64)
+run_shared=(.$install_dir/bin64/dontstarve_dedicated_server_nullrenderer_x64)
 run_shared+=(-console)
 run_shared+=(-cluster "$cluster_name")
 run_shared+=(-monitor_parent_process $$)
